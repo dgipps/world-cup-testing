1667aeaf84484a33cb54fe95aab9ee48e4727d78
Daniel Gipps <daniel.gipps@tophat.com>
2018-07-06 08:15:00 -0700

    Add country validator stub

--- app/models/team.model.js
+++ app/models/team.model.js
@@ -1,11 +1,11 @@
 var mongoose = require('mongoose');
-var validateFifaCode = require('./validators/team.validators.js').validateFifaCode;
+var validators = require('./validators/team.validators.js');
 var Schema = mongoose.Schema;
 
 // Team schema
 var TeamSchema = new Schema({
-    country: {type: String, required: true, index: { unique: true }},
-    fifa_code: {type: String, required: true, index: { unique: true }, validate: [ validateFifaCode, "Fifa Code is invalid."]},
+    country: {type: String, required: true, index: { unique: true }, validate: [ validators.validateQualifiedCountry, "Country did not qualify."]},
+    fifa_code: {type: String, required: true, index: { unique: true }, validate: [ validators.validateFifaCode, "Fifa Code is invalid."]},
     group_id: Number,
     group_letter: String,
 });
--- app/models/validators/team.validators.js
+++ app/models/validators/team.validators.js
@@ -1,9 +1,53 @@
+QUALIFIED_COUNTRIES = [
+    'Russia',
+    'France',
+    'Portugal',
+    'Germany',
+    'Serbia',
+    'Poland',
+    'England',
+    'Spain',
+    'Belgium',
+    'Iceland',
+    'Brazil',
+    'Uruguay',
+    'Argentina',
+    'Colombia',
+    'Nigeria',
+    'Senegal',
+    'Egypt',
+    'Mexico',
+    'Costa Rica',
+    'Panama',
+    'Iran',
+    'Japan',
+    'South Korea',
+    'Saudi Arabia',
+    'Switzerland',
+    'Italy',
+    'Denmark',
+    'Croatia',
+    'Sweden',
+    'Northern Ireland',
+    'Greece',
+    'Australia',
+    'Peru',
+    'Morocco',
+]
+
 var validateFifaCode = function(val) {
     if (val.length == 3) {
-        return true
+        return true;
     }
-    return false
+    return false;
+}
+
+var validateQualifiedCountry = function(val) {
+    // stub
+
+    return true;
 }
 
 module.exports = {}
 module.exports.validateFifaCode = validateFifaCode
+module.exports.validateQualifiedCountry = validateQualifiedCountry
